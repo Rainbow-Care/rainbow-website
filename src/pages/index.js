c5566bdf5c02ace727bada07d4083521545c797f
chinmoyDn <chinmoy.debnath11@gmail.com>
2025-01-12 10:27:59 +0530

    feat: adds font config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,12 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
 import HeroSection from "@/components/HeroSection";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 `}
     >
       <HeroSection />
+      <h2>Heading 2</h2>
     </div>
   );
 }
