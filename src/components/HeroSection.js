4de6e687ca413cab241808570bb6c60d9fa3624e
Koushik Debnath <debnathkoushik605@gmail.com>
2025-01-27 09:04:51 +0530

    fix styling

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,12 @@
-import Button from "./Button";
-import Image from "next/image";
+import Button from './Button';
+import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <div class="grid grid-flow-col w-full">
-      <div class="flex flex-col w-2/5">
+    <div class='grid grid-flow-col w-full'>
+      <div class='flex flex-col w-2/5'>
         <div>
-          <h1 class="font-bold text-lg">
+          <h1 class='font-bold text-lg'>
             A place that makes our silver citizens happy
           </h1>
           <p>
@@ -14,48 +14,51 @@ const HeroSection = () => {
             and wards to unwind, socialize and learn with their peers
           </p>
         </div>
-        <div class="flex h-fit flex-row justify-between">
+        <div class='flex h-fit flex-row gap-x-1	items-stretch	 justify-between'>
           <Button
             props={{
-              text: "Schedule an appointment",
+              text: 'Schedule an appointment',
               textColorPrimary: false,
               backgroundPrimary: true,
             }}
           />
           <Button
             props={{
-              text: "Learn More",
+              text: 'Learn More',
               textColorPrimary: true,
               backgroundPrimary: false,
             }}
           />
         </div>
       </div>
-      <div class="grid w-3/5">
-        <div class="grid grid-cols-2 w-fit">
+      <div class='grid grid-flow-col w-3/5 py-4'>
+        <div class='flex flex-col h-fit w-full'>
+          <div>
+            <img
+              src='/images/RainbowDayCareHeroSectionImage1.svg'
+              alt='Logo of Rainbow Day Care'
+            />
+          </div>
+
+          <div>
+            <img
+              src='/images/RainbowDayCareHeroSectionImage3.svg'
+              alt='Logo of Rainbow Day Care'
+            />
+          </div>
+        </div>
+        <div class='flex flex-col w-fit'>
           <Image
-            src="/images/RainbowDayCareHeroSectionImage1.svg"
-            width={291}
-            height={212}
-            alt="Logo of Rainbow Day Care"
-          ></Image>
-          <Image
-            src="/images/RainbowDayCareHeroSectionImage2.svg"
+            src='/images/RainbowDayCareHeroSectionImage2.svg'
             width={108}
             height={144}
-            alt="Logo of Rainbow Day Care"
+            alt='Logo of Rainbow Day Care'
           ></Image>
           <Image
-            src="/images/RainbowDayCareHeroSectionImage3.svg"
-            width={203}
-            height={132}
-            alt="Logo of Rainbow Day Care"
-          ></Image>
-          <Image
-            src="/images/RainbowDayCareHeroSectionImage4.svg"
+            src='/images/RainbowDayCareHeroSectionImage4.svg'
             width={175}
             height={202}
-            alt="Logo of Rainbow Day Care"
+            alt='Logo of Rainbow Day Care'
           ></Image>
         </div>
       </div>
