26573249031380cfdea06d65f389fca2bcc18b6a
Koushik Debnath <debnathkoushik605@gmail.com>
2025-03-15 17:38:56 +0530

    fix: fixes eslint parsing error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    ...compat.config,
-    extends: ["next", "prettier"],
+    extends: ['next', 'prettier'],
   }),
 ];
 
