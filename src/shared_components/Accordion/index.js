30f0690fe707ee0c524ca0bdbfc5819a269edcbb
Koushik Debnath <debnathkoushik605@gmail.com>
2025-03-20 19:46:19 +0530

    feat: adds accordion component

---
+++ src/shared_components/Accordion/index.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './style.css';
+
+const Accordion = ({ list }) => {
+  const [openSections, setOpenSections] = useState(new Set());
+
+  const onClickHandler = (e) => {
+    let newOpenSections = new Set(openSections);
+    newOpenSections.has(e.target.getAttribute('data-id'))
+      ? newOpenSections.delete(e.target.getAttribute('data-id'))
+      : newOpenSections.add(e.target.getAttribute('data-id'));
+
+    setOpenSections(newOpenSections);
+  };
+  return (
+    <div className='flex flex-col gap-4'>
+      {list.map(({ question, answer }) => {
+        const isExpanded = openSections.has(question);
+
+        return (
+          <div
+            className='flex flex-row border rounded-lg p-4 justify-between gap-8'
+            key={question}
+          >
+            <div className='flex flex-col gap-4'>
+              <button
+                data-id={question}
+                className='accordion-item-title font-bold'
+                onClick={onClickHandler}
+              >
+                {question}
+              </button>
+              <div hidden={!isExpanded}>{answer}</div>
+            </div>
+            <span
+              data-id={question}
+              className={`self-center
+ accordion-icon ${isExpanded && `accordion-icon--rotated`}`}
+              onClick={onClickHandler}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Accordion;
