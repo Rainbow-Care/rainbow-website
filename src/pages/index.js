e940e0c44e16d775beeceb8ab573b2364fb6ec74
Koushik Debnath <debnathkoushik605@gmail.com>
2025-04-01 18:39:37 +0530

    update: adds the question section

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Overview from '@/components/Overview';
 import Team from '@/components/Team';
 import Questions from '@/components/Questions';
 import Contact from '@/components/Contact';
+import Questions from '@/components/Questions';
 
 export default function Home() {
   return (
