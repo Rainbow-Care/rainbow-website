96e2babf05cd597f2c688adfba7a9a8fb4328e05
chinmoyDn <chinmoy.debnath11@gmail.com>
2025-05-01 15:30:02 +0530

    feat: fixes youtube embed dimensions

--- src/components/ReasonToChoose.js
+++ src/components/ReasonToChoose.js
@@ -9,10 +9,12 @@ const ReasonToChoose = ({ className }) => {
         <h2 className='text-center md:text-left'>{TITLE}</h2>
         <div className='flex flex-col'>
           <p>{DESCRIPTION}</p>
-          <div className='min-h-48 mx-auto'>
+          <div className='min-h-[200px] mt-8 w-full mx-auto'>
             <iframe
-              width='560'
-              height='315'
+              style={{
+                aspectRatio: '16 / 9',
+                width: '100%',
+              }}
               src='https://www.youtube-nocookie.com/embed/9JV6FrDrbH8?si=uLwwtJh7bhvBH3Eu'
               title='YouTube video player'
               frameborder='0'
