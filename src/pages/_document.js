ed9b5166e2034dbc689498ad2410ccc59fee3515
Koushik Debnath <debnathkoushik605@gmail.com>
2025-05-01 15:07:32 +0530

    feat: adds title and meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta
+          name='description'
+          content='A daycare in the heart of Pune, safely designed for your elderly parents to unwind, socialise and learn with their peers.'
+        />
+        <meta
+          name='keywords'
+          content='adult day care, senior citizens day care'
+        />
+        <meta name='google-site-verification' content='...' />
       </Head>
-      <body className="antialiased">
+      <body className='antialiased'>
         <Main />
         <NextScript />
       </body>
