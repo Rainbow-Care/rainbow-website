8ec9ead465a299c64b2329e71aac546514fb43e6
Koushik Debnath <debnathkoushik605@gmail.com>
2026-02-27 22:14:23 +0530

    feat(footer): adds footer with styling for mobile screens

--- src/components/Sobati/sections/Footer.js
+++ src/components/Sobati/sections/Footer.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className='border flex flex-col items-center gap-4 pt-32 px-4 mb-32'>
-      <p>{'Sobati - Compassionate Care for the Elderly'}</p>
-      <h1 className='text-center'>
-        {'Specialized Geriatrics & Palliative Care OPD'}
-      </h1>
+    <div className='flex flex-col items-center gap-8 pt-32 px-4 pb-16'>
+      <div className='flex flex-col items-center'>
+        <h2>Schedule an Appointment</h2>
+        <p className='text-center'>
+          {'Take the first step towards compassionate, comprehensive care'}
+        </p>
+      </div>
+
+      <div className='flex flex-col gap-4 items-center'>
+        <div className='flex gap-2'>
+          <img className='my-auto h-6' src='/images/sobati/Call.svg' />
+          <p className='font-bold'>{'Call Us'}</p>
+          <p className='text-[#1E85A2] font-bold'>{'8605003575'}</p>
+        </div>
+        <div className='flex gap-2'>
+          <img className='my-auto h-6' src='/images/sobati/Location.svg' />
+          <p className='font-bold'>{'Visit Us'}</p>
+          <p className='text-[#1E85A2] font-bold'>
+            {'Rainbow Day Care, Khotrud'}
+          </p>
+        </div>
+      </div>
+
+      <p className='italic text-[#1E85A2] text-center'>{`"Because compassionate care makes all the difference."`}</p>
     </div>
   );
 }
